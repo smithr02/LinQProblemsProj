86a7faec0e84ff19e76da7f178bc8351d8a6c045
Ryan Smith <ryan_smith2@hotmail.com>
2020-05-11 15:57:25 -0500

    Finished problem number 3 for method GradesAveraged

--- LinqHomework.cs
+++ LinqHomework.cs
@@ -52,10 +52,64 @@ namespace LinQProblems
 
         public void GradesAveraged(List<string> classGrades)
         {
+            var grades = classGrades;
+            List<int> listOne = new List<int>();
+            List<int> listTwo = new List<int>();
+            List<int> listThree = new List<int>();
+            List<int> listFour = new List<int>();
+            int i = 0;
+            var splitGrade = grades.SelectMany(g => g.Split(','));
+            var intGrade = splitGrade.Select(s => Int32.Parse(s));
+            foreach (var grade in intGrade)
+            {
+                while (i <= 20)
+                {
+                    if (i < 5)
+                    {
+                        listOne.Add(grade);
+                        i++;
+                        break;
+                    }
+                    else if (i > 4 && i < 10)
+                    {
+                        listTwo.Add(grade);
+                        i++;
+                        break;
+                    }
+                    else if (i > 9 && i < 15)
+                    {
+                        listThree.Add(grade);
+                        i++;
+                        break;
+                    }
+                    else if (i > 14 && i < 20)
+                    {
+                        listFour.Add(grade);
+                        i++;
+                        break;
+                    }
+                }
+            }
+            var sListOne = listOne.OrderBy(s => s).Skip(1).Average();
+            Console.WriteLine("First student average is: " + sListOne + "\n");
 
+            var sListTwo = listTwo.OrderBy(s => s).Skip(1).Average();
+            Console.WriteLine("First student average is: " + sListTwo + "\n");
+
+            var sListThree = listThree.OrderBy(s => s).Skip(1).Average();
+            Console.WriteLine("First student average is: " + sListThree + "\n");
+
+            var sListFour = listFour.OrderBy(s => s).Skip(1).Average();
+            Console.WriteLine("First student average is: " + sListFour + "\n");
+
+            double classAverage = (sListOne + sListTwo + sListThree + sListFour) / 4;
+            Console.WriteLine("Class average is: " + classAverage + "\n");
+            Console.ReadLine();
         }
 
-        public void ShortenWord()
+    }
+
+    public void ShortenWord()
         {
 
         }
